65faf463763f632febb30cf03cc0358ebe5a6f55
abdulhamad222 <abdulhamad373@gmail.com>
2026-02-23 15:02:01 +0500

    xyz

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,22 @@
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSecition';
-import Footer from '@/components/Footer';
-import React from 'react';
-import AboutCard from '@/components/AboutCard';
-import OurProjects from '@/components/OurProjects';
-import InteriorDesign from '@/components/Interior';
-import HeroSection2 from '@/components/Hero2';
-import Contact from '@/components/Contact';
-import 'animate.css';
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSecition";
+import Footer from "@/components/Footer";
+import React from "react";
+import AboutCard from "@/components/AboutCard";
+import OurProjects from "@/components/OurProjects";
+import InteriorDesign from "@/components/Interior";
+import HeroSection2 from "@/components/Hero2";
+import Contact from "@/components/Contact";
+import "animate.css";
 
 const Main = () => {
   const properties = [
     { image: "/images/house2.jpg", title: "Luxury Villa", price: "1,200,000" },
-    { image: "/images/house3.jpg", title: "Downtown Apartment", price: "650,000" },
+    {
+      image: "/images/house3.jpg",
+      title: "Downtown Apartment",
+      price: "650,000",
+    },
   ];
 
   return (
@@ -22,12 +26,16 @@ const Main = () => {
 
       {/* About Section */}
       <section className="py-16 px-6 bg-[#fcf9cc]">
-        <div className='animate__animated animate__fadeIn'>
+        <div className="animate__animated animate__fadeIn">
           <h2 className="text-3xl md:text-4xl font-bold mb-6 text-center text-[#873e23]">
             About EstatePro
           </h2>
           <p className="text-base sm:text-lg md:text-xl text-center max-w-3xl mx-auto text-gray-700 mb-12 leading-relaxed">
-            At EstatePro, we specialize in connecting you with your dream property — whether it’s a cozy family home, a modern apartment, or a luxurious villa. Our mission is to simplify real estate for everyone by offering trusted listings, expert guidance, and a seamless property buying experience.
+            At EstatePro, we specialize in connecting you with your dream
+            property — whether it’s a cozy family home, a modern apartment, or a
+            luxurious villa. Our mission is to simplify real estate for everyone
+            by offering trusted listings, expert guidance, and a seamless
+            property buying experience.
           </p>
         </div>
 
